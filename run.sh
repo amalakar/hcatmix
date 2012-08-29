563ae28ea233d94276796da11e52d210caf66e2c
Arup Malakar <amalakar@gmail.com>
2012-08-29 21:12:30 +0000

    Fixed run file

--- run.sh
+++ run.sh
@@ -9,14 +9,13 @@ export CLASSPATH=$CLASSPATH:`find $HADOOP_HOME/ -name '*.jar' | paste -d ':' -s 
 #export CLASSPATH=$CLASSPATH:`find $HADOOP_HOME/conf | paste -d ':' -s -`
 export CLASSPATH=$CLASSPATH:`find $PIG_HOME/lib/ -name '*.jar' | paste -d ':' -s -`
 #export CLASSPATH=$CLASSPATH:`find $PIG_HOME/conf | paste -d ':' -s -`
-export CLASSPATH=$CLASSPATH:`find ./lib/ -name '*.jar' | paste -d ':' -s -`
+export CLASSPATH=$CLASSPATH:`find $PWD/lib/ -name '*.jar' | paste -d ':' -s -`
 
 conf_dir="${HADOOP_CONF_DIR:-$HADOOP_HOME/conf}"
 scripts=`dirname "$0"`
 scripts=`cd $scripts; pwd`
 pigperfjar=$scripts/pigperf.jar
 
-export HADOOP_CLASSPATH=$pigperfjar:$CLASSPATH
-echo "hadoop  --config $conf_dir jar $pigperfjar org.apache.hcatalog.hcatmix.HiveTableCreator -f scripts/hcat_table_specification.xml -m 1 -o /user/malakar/hcatmix/" 
-hadoop  --config $conf_dir jar $pigperfjar org.apache.hcatalog.hcatmix.HiveTableCreator -f scripts/hcat_table_specification.xml -m 1 -o /user/malakar/hcatmix/ 
+export HADOOP_CLASSPATH=$pigperfjar:$PWD/lib/sdsuLibJKD12.jar:$CLASSPATH
+hadoop  --config $conf_dir jar $pigperfjar  org.apache.hcatalog.hcatmix.HiveTableCreator -libjars $PWD/lib/sdsuLibJKD12.jar -f scripts/hcat_table_specification.xml -m 1 -o /user/malakar/hcatmix/ 
 #hadoop  org.apache.hcatalog.hcatmix.HiveTableCreator -f scripts/hcat_table_specification.xml -m 0 -o /tmp/hcatmix/ 
