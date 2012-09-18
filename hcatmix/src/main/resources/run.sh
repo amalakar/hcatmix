5666d7e579404f614c7ba0501d81b8b3703fd894
Arup Malakar <amalakar@gmail.com>
2012-09-18 18:09:21 +0000

    pom added jar with dependencies

--- hcatmix/src/main/resources/run.sh
+++ hcatmix/src/main/resources/run.sh
@@ -9,13 +9,13 @@ export CLASSPATH=$CLASSPATH:`find $HADOOP_HOME/ -name '*.jar' | paste -d ':' -s 
 #export CLASSPATH=$CLASSPATH:`find $HADOOP_HOME/conf | paste -d ':' -s -`
 export CLASSPATH=$CLASSPATH:`find $PIG_HOME/lib/ -name '*.jar' | paste -d ':' -s -`
 #export CLASSPATH=$CLASSPATH:`find $PIG_HOME/conf | paste -d ':' -s -`
-export CLASSPATH=$CLASSPATH:`find $PWD/lib/ -name '*.jar' | paste -d ':' -s -`
+#export CLASSPATH=$CLASSPATH:`find $PWD/lib/ -name '*.jar' | paste -d ':' -s -`
 
 conf_dir="${HADOOP_CONF_DIR:-$HADOOP_HOME/conf}"
 scripts=`dirname "$0"`
-scripts=`cd $scripts; pwd`
-pigperfjar=$scripts/pigperf.jar
+scripts=`cd $scripts/../../../; pwd`
+hcatmixjar=$scripts/target/hcatmix-1.0-SNAPSHOT-jar-with-dependencies.jar
 
 export HADOOP_CLASSPATH=$pigperfjar:$PWD/lib/sdsuLibJKD12.jar:$CLASSPATH
-hadoop  --config $conf_dir jar $pigperfjar  org.apache.hcatalog.hcatmix.HiveTableCreator -libjars $PWD/lib/sdsuLibJKD12.jar -f scripts/hcat_table_specification.xml -m 0 -o /user/malakar/hcatmix/ 
+hadoop  --config $conf_dir jar $hcatmixjar org.apache.hcatalog.hcatmix.HiveTableCreator -f src/main/resources/hcat_table_specification.xml -m 0 -o /tmp/data 
 #hadoop  org.apache.hcatalog.hcatmix.HiveTableCreator -f scripts/hcat_table_specification.xml -m 0 -o /tmp/hcatmix/ 
