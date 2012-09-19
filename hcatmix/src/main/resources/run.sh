33300f0ce967f82298ddbc522929c6fb106eedb8
Arup Malakar <amalakar@gmail.com>
2012-09-19 21:34:08 +0000

    More argument for the run script

--- hcatmix/src/main/resources/run.sh
+++ hcatmix/src/main/resources/run.sh
@@ -17,5 +17,5 @@ scripts=`cd $scripts/../../../; pwd`
 hcatmixjar=$scripts/target/hcatmix-1.0-SNAPSHOT-jar-with-dependencies.jar
 
 export HADOOP_CLASSPATH=$pigperfjar:$PWD/lib/sdsuLibJKD12.jar:$CLASSPATH
-hadoop  --config $conf_dir jar $hcatmixjar org.apache.hcatalog.hcatmix.HiveTableCreator -f src/main/resources/hcat_table_specification.xml -m 0 -o /tmp/data 
+hadoop  --config $conf_dir jar $hcatmixjar org.apache.hcatalog.hcatmix.HiveTableCreator -f src/main/resources/hcat_table_specification.xml -m 1 -o /tmp/data -s -p /tmp/pig
 #hadoop  org.apache.hcatalog.hcatmix.HiveTableCreator -f scripts/hcat_table_specification.xml -m 0 -o /tmp/hcatmix/ 
